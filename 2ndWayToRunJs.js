1a24756b791330c561005ef9f921c403a4b74fb9
Jiwonieee19 <ryanjaycompuesto@gmail.com>
2026-01-09 22:20:20 +0800

    day2, small progress coz bc irl

---
+++ 2ndWayToRunJs.js
@@ -0,0 +1,5 @@
+console.log("From other world")
+
+//called inside the runningJsCode as sample
+
+//to run this on backend, type node .\name.js, make sure node.js is already installed
---
+++ consoleOutputsAndComments.js
@@ -0,0 +1,12 @@
+console.log("Normal Text can also include int: ", 15)
+console.error("Red Text")
+console.warn("Yellow Text")
+
+const v = 615
+
+console.log(`V = ${v}`) //backticks here act the same as printf on java
+console.log("V = ${v}") //if inside a normal double qoutations, it will return a string V = ${v}
+
+//for single comment
+/* for multi
+    comments */
